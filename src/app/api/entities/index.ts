fc900bf304649dc7d56417da2445a7be4a2118ec
Sergio García <3311638+serchtul@users.noreply.github.com>
2025-03-13 00:42:46 -0600

    draft initial mock of election tracker

---
+++ src/app/api/entities/index.ts
@@ -0,0 +1,21 @@
+import type { Entity } from "@/store/ballot";
+
+export const eys: Entity[] = [
+  { id: '', name: 'Aguascalientes', code: 'AGS', votingState: 'voting' },
+  { id: '', name: 'Ciudad Juárez', code: 'JRZ', votingState: 'voting' },
+  { id: '', name: 'Guadalajara', code: 'GDL', votingState: 'done' },
+  { id: '', name: 'Guanajuato', code: 'GTO', votingState: 'voting' },
+  { id: '', name: 'IPN', code: 'IPN', votingState: 'voting' },
+  { id: '', name: 'ITAM', code: 'ITA', votingState: 'done' },
+  { id: '', name: 'Michoacán', code: 'MCH', votingState: 'offline' },
+  { id: '', name: 'Monterrey', code: 'MTY', votingState: 'done' },
+  { id: '', name: 'Oaxaca', code: 'OAX', votingState: 'voting' },
+  { id: '', name: 'Puebla', code: 'PUE', votingState: 'done' },
+  { id: '', name: 'Querétaro', code: 'QRO', votingState: 'voting' },
+  { id: '', name: 'Sinaloa', code: 'SIN', votingState: 'voting' },
+  { id: '', name: 'Toluca', code: 'TOL', votingState: 'offline' },
+  { id: '', name: 'UNAM', code: 'UNA', votingState: 'done' },
+  { id: '', name: 'UP', code: 'UP', votingState: 'offline' },
+  { id: '', name: 'Zacatecas', code: 'ZAC', votingState: 'done', votes: 2 },
+  { id: '', name: 'Comité Nacional', code: 'MC', votingState: 'done' },
+]
---
+++ src/components/entity-card.tsx
@@ -0,0 +1,43 @@
+import type { Entity, Status } from "@/store/ballot";
+import { Card, CardContent } from "./ui/card";
+import { CheckCircle, WifiOff } from 'lucide-react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils";
+
+const statusMessages: Record<Status, string> = {
+  offline: 'Desconectado',
+  voting: 'Votando',
+  done: 'Votó',
+}
+
+export default function EntityCard({name, votingState: status, code}: Entity) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Card className="flex w-20 lg:w-25 aspect-5/3 justify-center items-center">
+            <CardContent className={cn(
+              "flex items-center gap-1",
+              status === 'offline' && 'text-zinc-500'
+            )}>
+              {status === 'offline' && <WifiOff />}
+              {status === 'done' && <CheckCircle className="text-green-500" />}
+              {code}
+            </CardContent>
+          </Card>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{name} - {statusMessages[status]}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+    
+    
+    
+}
