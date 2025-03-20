8baaea81e77997332a96570d737929f6c911bc05
Sergio García <3311638+serchtul@users.noreply.github.com>
2025-03-19 18:43:12 -0600

    add error message when user is not assigned to election

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { auth } from "@/auth";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import VotingForm from "@/components/voting-form";
 import { db } from "@/db";
 import { plainify } from "@/lib/plain-ify";
 import type { Election, Entity } from "@/store/ballot";
+import { AlertCircle } from "lucide-react";
 import { headers } from "next/headers";
 
 export default async function Home() {
@@ -29,19 +31,32 @@ export default async function Home() {
       .innerJoin("electionVoter", "entity.id", "entityId")
       .select(["id", "name", "code", "votingStatus", "votes"])
       .where("votingEmail", "=", email)
-      .executeTakeFirstOrThrow(),
-  ) as Entity;
+      .executeTakeFirst(),
+  );
+
+  if (!entity) {
+    return (
+      <div className="flex justify-center">
+        <Alert variant="destructive" className="w-fit">
+          <AlertCircle className="size-4" />
+          <AlertTitle className="font-bold">Error</AlertTitle>
+          <AlertDescription>
+            Tu usuario no está asignado a ninguna elección, intenta nuevamente.
+            <br /> Si el problema persiste, contacta al administrador.
+          </AlertDescription>
+        </Alert>
+      </div>
+    );
+  }
 
   const election: Election = {
     ...dbElection,
     candidates,
   };
 
-  // TODO: Add different branches for when an entity has is done voting (maybe a redirect?)
   return (
     <>
-      <h1>Votaciones</h1>
-      <VotingForm election={election} entity={entity} />
+      <VotingForm election={election} entity={entity as Entity} />
     </>
   );
 }
--- src/lib/plain-ify.ts
+++ src/lib/plain-ify.ts
@@ -1,5 +1,9 @@
 // TODO: Explain why this function (tl;dr: It's a workaround for a libsql serialization issue)
 export function plainify<T>(object: T): T {
+  if (!object) {
+    return object;
+  }
+
   // Spreading doesn't work because nested objects can also have hidden properties
   return JSON.parse(JSON.stringify(object)) as T;
 }
